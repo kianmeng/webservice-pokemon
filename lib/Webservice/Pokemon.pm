22424c979d6dfa36214466fe83b2b657fbfb0378
Kian-Meng, Ang <kianmeng@users.noreply.github.com>
2018-05-05 10:43:43 +0800

    Add and generate initial README

--- lib/Webservice/Pokemon.pm
+++ lib/Webservice/Pokemon.pm
@@ -3,5 +3,38 @@ package Webservice::Pokemon;
 
 use strict;
 use warnings;
+use utf8
+
 
 1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Webservice::Pokemon - A Perl module to interface with the Pokemon API
+(pokeapi.co) webservice.
+
+=head1 SYNOPSIS
+
+    use Webservice::Swapi;
+
+    $swapi = Webservice::Swapi->new;
+
+=head1 DESCRIPTION
+
+Webservice::Pokemon is a Perl client helper library for the Pokemon API (pokeapi.co).
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is Copyright (c) 2017 by Kian Meng, Ang.
+
+This is free software, licensed under:
+
+    The Artistic License 2.0 (GPL Compatible)
+
+=head1 AUTHOR
+
+Kian-Meng, Ang E<lt>kianmeng@users.noreply.github.comE<gt>
+
