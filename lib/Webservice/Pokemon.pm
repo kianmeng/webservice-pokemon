2c0fad0d797c1176316745ef122bbdb6e90c83f3
Kian-Meng, Ang <kianmeng@users.noreply.github.com>
2018-05-05 15:22:38 +0800

    More documentation on the development steps

--- lib/Webservice/Pokemon.pm
+++ lib/Webservice/Pokemon.pm
@@ -36,9 +36,24 @@ Webservice::Pokemon is a Perl client helper library for the Pokemon API (pokeapi
 
 =head1 DEVELOPMENT
 
+Setting up the required packages.
+
     $ cpanm Dist::Milla
     $ milla listdeps --missing | cpanm
+
+Check you code coverage.
+
+    $ milla cover
+
+Several ways to run the test.
+
     $ milla test
+    $ milla test --release
+    $ RELEASE_TESTING=1 milla test
+    $ RELEASE_TESTING=1 milla run prove t/01_instantiation.t
+
+Release the module.
+
     $ milla build
     $ milla release
 
