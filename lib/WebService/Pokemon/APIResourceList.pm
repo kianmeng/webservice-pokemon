0cef6319abc9882266a3c6ae2e45b8bb77e97c6a
Kian Meng, Ang <kianmeng@cpan.org>
2018-06-10 00:21:43 +0800

    Remove code redundancy using Moo::Role

--- lib/WebService/Pokemon/APIResourceList.pm
+++ lib/WebService/Pokemon/APIResourceList.pm
@@ -5,38 +5,10 @@ use strictures 2;
 use namespace::clean;
 
 use Moo;
-use Types::Standard qw(HashRef);
 
+with 'WebService::Pokemon::Role::APIResource';
 
-has api => (
-    is => 'rw',
-);
-
-has response => (
-    isa => HashRef,
-    is  => 'rw',
-);
-
-sub _build_results {
-    my $self = shift;
-}
-
-sub BUILD {
-    my ($self, $args) = @_;
-
-    foreach my $arg (keys %$args) {
-        $self->$arg($args->{$arg}) if (defined $args->{$arg});
-    }
-
-    foreach my $arg (keys %{$self->response}) {
-        if (!$self->can($arg)) {
-            $self->meta->add_attribute($arg => ( is => 'rw'));
-            $self->$arg($self->response->{$arg});
-        }
-    }
-
-    return $self;
-}
+our $VERSION = '0.08';
 
 
 1;
